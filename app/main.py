4a6de39f0344a3473cbc28f78034147010b3c164
Jim Tyhurst <jim@tyhurst.com>
2023-06-22 23:10:21 -0700

    Fixed duplicate use of function name.

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ def generate_greeting(name=""):
 
 
 @app.get("/greeting")
-async def greeting():
+async def generic_greeting():
     return {"message": generate_greeting()}
 
 
 @app.get("/greeting/{name}")
-async def greeting(name):
+async def greeting_by_name(name):
     return {"message": generate_greeting(name)}
